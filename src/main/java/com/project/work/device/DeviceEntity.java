8487667f2f4c9c4bd05a4a0c9b20cb71e6b33163
Md Shuphiyan <md.shuphiyan@gmail.com>
2020-09-09 20:39:32 +0530

    seggreagated model

--- src/main/java/com/project/work/device/DeviceEntity.java
+++ src/main/java/com/project/work/device/DeviceEntity.java
@@ -3,13 +3,31 @@ package com.project.work.device;
 import com.project.work.entities.AbstractBaseEntity;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Data
+@NoArgsConstructor
 @Builder
 @Entity
+@Table(name = "device")
 public class DeviceEntity extends AbstractBaseEntity {
-    private String attribute1;
-    private String attribute2;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "device_type")
+    private String device_type;
+
+    @Column(name = "manufacturer")
+    private String manufacturer;
+
+    @Column(name = "device_mgr_id")
+    private int device_mgr_id;
+
+    @Column(name = "location_id")
+    private int location_id;
 }
--- src/main/java/com/project/work/entities/AbstractBaseEntity.java
+++ src/main/java/com/project/work/entities/AbstractBaseEntity.java
@@ -1,16 +1,29 @@
 package com.project.work.entities;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.Version;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @MappedSuperclass
+@Data
 public class AbstractBaseEntity implements Serializable {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "last_updated_time")
+    private Date last_updated_time;
 
     @Version
     private int version;
